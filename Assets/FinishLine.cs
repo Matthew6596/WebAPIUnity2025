ec991f8edfef66b27951ad96c2f3269ed38589f8
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2025-03-09 21:43:14 -0400

    Added racing scripts and scenes

---
+++ Assets/FinishLine.cs
@@ -0,0 +1,29 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isServer) return;
+
+        PlayerRacer racer = other.gameObject.GetComponent<PlayerRacer>();
+        if (racer == null) return;
+
+        racer.FinishRace();
+    }
+}
