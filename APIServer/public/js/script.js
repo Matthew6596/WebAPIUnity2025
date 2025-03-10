fdfc529a3ca1c489eeecd724dda07d1daf4c4019
rft0202 <rftester@email.neit.edu>
2025-03-09 21:10:28 -0400

    added top ten fetch

--- APIServer/public/js/script.js
+++ APIServer/public/js/script.js
@@ -1,6 +1,6 @@
 listContainer = document.getElementById("listContainer");
 
-const fetchPlayerData= async ()=>{
+const fetchPlayerData = async ()=>{
     try{
         //fetch data from server
         const response = await fetch("/player"); //get all players
@@ -22,8 +22,44 @@ const fetchPlayerData= async ()=>{
         });
     } catch(error){
         console.error("Error: ", error);
-        listContainer.innerHTML = "<p style='color:red'>Failed to get playyer data</p>";
+        listContainer.innerHTML = "<p style='color:red'>Failed to get player data</p>";
     }
 }
 
-fetchPlayerData();
+const fetchTopTenPlayers = async ()=>{
+    try{
+        //fetch data from server
+        const response = await fetch("/playersByWins"); //get all players sorted by wins
+        if(!response.ok){
+            throw new Error("Failed to get players by wins");
+        }
+
+        //Parse
+        const players = await response.json();
+
+        listContainer.innerHTML = "";
+
+        //Add top 10 to list
+        for (let i = 0; i < 10; i++){
+            var player;
+
+            if(players[i] != null){
+                player = players[i];
+            }
+            else{
+                break;
+            }
+
+            const listDiv = document.createElement("div");
+            listDiv.className = "player";
+            listDiv.innerHTML = `${i + 1}. ${player.username} ${player.besttime} ${player.wincount} ${player.gamesplayed}`;
+            listContainer.appendChild(listDiv);
+        };
+    } catch(error){
+        console.error("Error: ", error);
+        listContainer.innerHTML = "<p style='color:red'>Failed to get player data</p>";
+    }
+}
+
+//fetchPlayerData();
+fetchTopTenPlayers();
